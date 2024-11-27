d768ba876b31c79b33695a2ff29612d2e06b234a
Silke pilon <silkepilon2009@gmail.com>
2024-11-27 15:44:24 +0100

    Comment out TeacherRoster component in Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,13 @@
-import AdvancedCountdown from '../components/AdvancedCountdown'
-import TeacherRoster from '../components/TeacherRoster'
+import AdvancedCountdown from "../components/AdvancedCountdown";
+import TeacherRoster from "../components/TeacherRoster";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-gray-100 to-gray-200 dark:from-gray-800 dark:to-gray-900 p-4">
       <div className="w-full max-w-4xl">
         <AdvancedCountdown />
-        <TeacherRoster />
+        {/* <TeacherRoster /> */}
       </div>
     </main>
-  )
+  );
 }
-
